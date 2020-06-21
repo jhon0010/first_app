a6f15b01e599269dc2a13087f8fe703d3da594e9
jhon0010 <jhoning.soft@gmail.com>
2020-06-21 18:58:12 -0500

    Arrange code

---
+++ lib/practice/foreach.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import '../ListFavorites.dart';
+
+void main() => runApp(ForEachExample());
+
+class ForEachExample extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    var ages = [33, 15, 27, 40, 22];
+    ages.sort();
+    var sum = 0;
+    var minor = ages.take(1);
+    var major = ages.elementAt(ages.length - 1);
+    ages.forEach((age) => sum += age);
+    var average = sum / ages.length;
+
+    print(
+        'The minus number $minor \n The major is  $major \n The average is $average');
+
+
+    return MaterialApp(
+
+      debugShowCheckedModeBanner: false,
+      title: 'Welcome to flutter',
+      theme: ThemeData(primaryColor: Colors.green),
+      home: FavoriteList(),
+    );
+  }
+}
